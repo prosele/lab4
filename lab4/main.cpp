53d910e1f29c606678f22765d86dd98c4a5416b7
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2023-10-16 11:34:40 +0300

    Initial Commit

---
+++ lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Ксения Прокопович on 16.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
